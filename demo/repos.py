f05ba38575c86d8f49c1b2a6e06d71c821967ad3
Elia Giacobazzi <elia.giacobazzi@euler.it>
2021-08-10 18:39:54 +0200

    Create repository for frequencies storage

---
+++ demo/repos.py
@@ -0,0 +1,30 @@
+from typing import List
+
+from .geo import Coordinates, find_nearest_place
+
+
+class FrequencyRepo:
+    # TODO: places should be initialized by a method executed during app initialization
+    # TODO: with bisect module and custom structure it's possible to improve performance
+    # TODO: frequencies should be saved in an external database
+
+    def __init__(self):
+        self._places = [(44.6543412, 10.9011459)]
+        self._frequencies = {coord: dict(test=1) for coord in self._places}
+
+    @property
+    def places(self) -> List[Coordinates]:
+        return self._places
+
+    def find_nearest_place(self, coord: Coordinates):
+        return find_nearest_place(coord, self._places)
+
+    def update_frequency(self, place: Coordinates, key: str):
+        frequency_table = self._frequencies[place]
+        try:
+            frequency_table[key] += 1
+        except KeyError:
+            frequency_table[key] = 1
+
+    def fetch_frequency_table(self, place: Coordinates):
+        return self._frequencies[place]
