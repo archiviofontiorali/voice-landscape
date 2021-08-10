f289333e0d9787d538cb99eaf611ee1bf6e7bde3
Elia Giacobazzi <elia.giacobazzi@euler.it>
2021-08-10 18:39:22 +0200

    Create a geo module

---
+++ demo/geo/__init__.py
@@ -0,0 +1,17 @@
+import math
+from typing import List, Tuple
+
+import geopy.distance
+
+Coordinates = Tuple[float, float]
+
+
+def find_nearest_place(
+    target: Coordinates, places: List[Coordinates]
+) -> Tuple[Coordinates, float]:
+    near, distance = None, math.inf
+    for place in places:
+        new_distance = geopy.distance.geodesic(target, place)
+        if new_distance < distance:
+            near, distance = place, new_distance
+    return near, distance
