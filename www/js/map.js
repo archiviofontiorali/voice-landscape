82dc4986739c93a01c3862be1be8a84760224eef
Elia Giacobazzi <elia.giacobazzi@euler.it>
2021-09-16 13:33:26 +0200

    Improve font and colors

--- www/js/map.js
+++ www/js/map.js
@@ -12,7 +12,8 @@ const options = {
   backgroundColor: "rgba(255, 255, 255, 0)", 
   shrinkToFit: true,
   fontFamily: "Consolas, monaco, monospace",
-  color: "rgba(0, 0, 0, .8)",
+  // color: "rgba(0, 0, 0, .8)",
+  color: "red",
 };
 
 
