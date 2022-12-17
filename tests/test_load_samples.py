df94cec811e8e59f9e3f3fb11907526594d5f53c
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2022-12-17 05:24:19 +0100

    Add test to load some sample in library

---
+++ tests/test_load_samples.py
@@ -0,0 +1,18 @@
+import numpy as np
+from starlette.testclient import TestClient
+
+import demo.app
+import demo.constants as c
+
+app = demo.app.App().app()
+client = TestClient(app)
+
+
+def test_app():
+    for x, y in np.random.rand(100, 2):
+        coord = c.a + x * c.v1 + y * c.v2
+        response = client.post(
+            url="/share",
+            data={"loc-x": coord[0], "loc-y": coord[1], "text": "Paesaggio di Voci"},
+        )
+        assert response.status_code == 200, response
