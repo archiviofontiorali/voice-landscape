1bd9cbf9c56950393e416a1595505ddedfdbb3a3
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2023-03-10 01:14:18 +0100

    Add decorator abstractmethod and create a GenericHandler interface

--- voices/system/types.py
+++ voices/system/types.py
@@ -1,19 +1,25 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import Any
 
 from starlette.requests import Request
 
 
 class Case(ABC):
+    @abstractmethod
     async def execute(self, request: Request, presenter) -> dict:
         pass
 
 
-class Handler(ABC):
+class GenericHandler(ABC):
+    @abstractmethod
+    async def __call__(self, request: Request):
+        pass
+
+
+class Handler(GenericHandler, ABC):
     case: Case
+    presenter: Any
 
     def __init__(self, case: Case, presenter):
         self.case = case
         self.presenter = presenter
-
-    async def __call__(self, request: Request):
-        pass
