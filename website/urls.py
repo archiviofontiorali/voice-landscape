4eea27b1e43991a244d2aff081f3f6c6a1d3c602
Elia Giacobazzi <elia.giacobazzi@euler.it>
2023-03-13 16:05:32 +0100

    Add page views

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.HomePage.as_view(), name="index"),
+    path("", views.HomePage.as_view(), name="home"),
+    path("map", views.MapPage.as_view(), name="map"),
+    path("share", views.SharePage.as_view(), name="share"),
+    path("showcase", views.ShowcasePage.as_view(), name="showcase"),
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,25 @@ from django.views import generic
 
 class HomePage(generic.TemplateView):
     template_name = "home.html"
+
+
+class SharePage(generic.TemplateView):
+    template_name = "share.html"
+
+
+class MapPage(generic.TemplateView):
+    template_name = "map.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context.setdefault("places", [])
+        return context
+
+
+class ShowcasePage(generic.TemplateView):
+    template_name = "showcase.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context.setdefault("places", [])
+        return context
