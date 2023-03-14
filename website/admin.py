d524266bfdfff476b5675fcae9c5cfd773b91a09
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2023-03-14 03:12:29 +0100

    Add model place and wordfrequency

--- website/admin.py
+++ website/admin.py
@@ -9,5 +9,21 @@ class ShareAdmin(admin.GISModelAdmin):
 
 
 @admin.register(models.Landscape)
-class ShareAdmin(admin.GISModelAdmin):
-    pass
+class LandscapeAdmin(admin.GISModelAdmin):
+    list_display = ("title", "center")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(models.Place)
+class PlaceAdmin(admin.GISModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+    def location(self):
+        return f"({self.location.y:.6f}, {self.location.x:.6f})"
+
+    list_display = ("title", "location", "description")
+
+
+@admin.register(models.WordFrequency)
+class WordFrequencyAdmin(admin.GISModelAdmin):
+    list_display = ("word", "place", "frequency")
--- website/models.py
+++ website/models.py
@@ -11,3 +11,26 @@ class Landscape(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
     center = models.PointField()
+
+
+class Place(models.Model):
+    slug = models.SlugField(unique=True, blank=True)
+    title = models.CharField(max_length=100, blank=True)
+    description = models.TextField(max_length=500, blank=True)
+    location = models.PointField()
+
+    def __str__(self):
+        if self.title:
+            return self.title
+        if self.slug:
+            return self.slug
+        return f"({self.location.y:.6f}, {self.location.x:.6f})"
+
+
+class WordFrequency(models.Model):
+    word = models.CharField(max_length=100)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    frequency = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        verbose_name_plural = "Word Frequencies"
