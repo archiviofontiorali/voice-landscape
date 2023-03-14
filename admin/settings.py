ec6e89261e08ff0bbf20f9f5006610de4922efb8
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2023-03-14 01:35:30 +0100

    Enable GeoDjango and move to spatialite sqlite engine

--- admin/settings.py
+++ admin/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.gis",
     "sass_processor",
 ]
 
@@ -82,9 +83,13 @@ WSGI_APPLICATION = "admin.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.contrib.gis.db.backends.spatialite",
         "NAME": BASE_DIR / "db.sqlite3",
-    }
+    },
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # },
 }
 
 
