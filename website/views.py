dedbc781a97e9829b570b2a100043b8ff59bffe7
Elia Giacobazzi <elia.giacobazzi@euler.it>
2023-03-15 16:18:05 +0100

    Move voices module logics inside MapContextMixin

--- website/views.py
+++ website/views.py
@@ -1,6 +1,38 @@
+from django.db.models import Max
 from django.views import generic
 
-from . import voices
+from . import models
+
+
+class MapContextMixin:
+    @staticmethod
+    def _calculate_centroid():
+        places = models.Place.objects.all()
+        return [
+            sum(p.location.y for p in places) / len(places) if places else 0.0,
+            sum(p.location.x for p in places) / len(places) if places else 0.0,
+        ]
+
+    @staticmethod
+    def _fetch_place_frequencies(place: models.Place):
+        query = models.WordFrequency.objects.filter(place=place)
+        max_frequency = query.aggregate(Max("frequency"))["frequency__max"]
+
+        obj = {"coordinates": place.coordinates, "frequencies": {}}
+        for wf in query.all():
+            obj["frequencies"][wf.word] = wf.frequency / max_frequency
+
+        return obj
+
+    def get_context_data(self):
+        context = {
+            "center": self._calculate_centroid(),
+            "places": [
+                self._fetch_place_frequencies(place)
+                for place in models.Place.objects.all()
+            ],
+        }
+        return context
 
 
 class HomePage(generic.TemplateView):
@@ -11,25 +43,9 @@ class SharePage(generic.TemplateView):
     template_name = "share.html"
 
 
-class MapPage(generic.TemplateView):
+class MapPage(MapContextMixin, generic.TemplateView):
     template_name = "map.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data()
 
-        context.setdefault("places", voices.extrapolate_place_word_frequencies())
-        context.setdefault("center", voices.calculate_places_centroid())
-
-        return context
-
-
-class ShowcasePage(generic.TemplateView):
+class ShowcasePage(MapContextMixin, generic.TemplateView):
     template_name = "showcase.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data()
-
-        context.setdefault("places", voices.extrapolate_place_word_frequencies())
-        context.setdefault("center", voices.calculate_places_centroid())
-
-        return context
