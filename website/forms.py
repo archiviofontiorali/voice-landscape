1edfd5be3f20c03cee11d00e6561cf2b68bb5120
Elia Giacobazzi <elia.giacobazzi@euler.it>
2023-03-18 18:33:13 +0100

    Convert form to base form and add location fields

--- website/forms.py
+++ website/forms.py
@@ -1,19 +1,18 @@
 import django.forms
+from django.contrib.gis.forms import CharField, DecimalField, Textarea
 from django.utils.translation import gettext as _
 
-from . import models
 
+class ShareForm(django.forms.Form):
+    message = CharField(
+        label=_("Esprimi la tua voce"),
+        help_text=_(
+            "Scrivi il tuo pensiero qui o usa il tasto microfono per "
+            "registrare la tua voce"
+        ),
+        widget=Textarea,
+        max_length=500,  # TODO: find a way to use same value in form and model
+    )
 
-class ShareForm(django.forms.ModelForm):
-    class Meta:
-        model = models.Share
-        fields = ["message", "location"]
-        labels = {
-            "message": _("Esprimi la tua voce"),
-        }
-        help_texts = {
-            "message": _(
-                "Scrivi il tuo pensiero qui o usa il tasto microfono per registrare "
-                "la tua voce"
-            ),
-        }
+    latitude = DecimalField(label=_("Latitudine"))
+    longitude = DecimalField(label=_("Longitudine"))
