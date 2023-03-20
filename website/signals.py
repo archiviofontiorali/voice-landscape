6ebdde748499bcea2d502380d4ed36e191f26e8b
Elia Giacobazzi <elia.giacobazzi@euler.it>
2023-03-20 18:47:08 +0100

    Add logs to signals.py

--- website/signals.py
+++ website/signals.py
@@ -1,6 +1,5 @@
 import spacy.symbols
 from django.conf import settings
-from django.contrib.gis.db.models.functions import Distance
 from django.db.models import F
 from loguru import logger
 
@@ -9,6 +8,7 @@ from . import models
 try:
     nlp = spacy.load(settings.SPACY_MODEL_NAME)
 except IOError:
+    logger.info("Spacy model not found downloading it")
     spacy.cli.download(settings.SPACY_MODEL_NAME)
     nlp = spacy.load(settings.SPACY_MODEL_NAME)
 
@@ -16,16 +16,27 @@ except IOError:
 def on_share_creation_update_frequencies(
     sender, instance: models.Share, created, **kwargs
 ):
-    place = (
-        models.Place.objects.annotate(distance=Distance("location", instance.location))
-        .order_by("distance")
-        .first()
+    if not created:
+        return
+
+    place = models.Place.get_nearest(instance.location)
+    logger.debug(
+        f"Add share near '{place}', try to update frequencies "
+        f"(message: {instance.message})"
     )
 
     for token in nlp(instance.message):
-        if token.pos in settings.SPACY_VALID_TOKENS:
-            wf, created = models.WordFrequency.objects.get_or_create(
-                place=place, word=token.lemma_.lower()
-            )
-            wf.frequency = F("frequency") + 1
-            wf.save()
+        message = f"token {token} with pos ({token.pos}) '{spacy.explain(token.pos_)}'"
+        if token.pos not in settings.SPACY_VALID_TOKENS:
+            logger.debug(f"Skip {message}")
+            continue
+
+        word = token.lemma_
+        wf, created = models.WordFrequency.objects.get_or_create(place=place, word=word)
+        wf.frequency = F("frequency") + 1
+        wf.save()
+
+        if created:
+            logger.debug(f"Found {message}, create counter for {word}")
+        else:
+            logger.debug(f"Found {message}, increment counter for {word}")
