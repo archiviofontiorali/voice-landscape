260a8aff305dc4d98d23feb24c8509a57e2ae68b
Elia Giacobazzi <elia.giacobazzi@euler.it>
2023-03-24 16:39:21 +0100

    Make STATIC_ROOT .env configurable

--- admin/settings.py
+++ admin/settings.py
@@ -18,7 +18,7 @@ from decouple import config  # noqa
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR: Path = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = BASE_DIR / ".static"
+
 LOG_ROOT = BASE_DIR / ".log"
 DATA_ROOT = BASE_DIR / ".data"
 
@@ -133,6 +133,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_ROOT: Path = config("STATIC_ROOT", default=BASE_DIR / ".static")
+
 
 STATIC_URL = "static/"
 
