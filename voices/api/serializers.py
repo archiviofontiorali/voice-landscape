97de985c604bb65bf2865c5d6d4880cea4cc9463
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-04-28 01:07:19 +0200

    Add url field in api serializers

--- voices/api/serializers.py
+++ voices/api/serializers.py
@@ -6,16 +6,16 @@ from ..website.models import Place, Share, WordFrequency
 class PlaceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Place
-        fields = ["slug", "title", "description", "coordinates"]
+        fields = ["url", "slug", "title", "description", "coordinates"]
 
 
 class ShareSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Share
-        fields = ["timestamp", "coordinates", "message"]
+        fields = ["url", "timestamp", "coordinates", "message"]
 
 
 class WordFrequencySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = WordFrequency
-        fields = ["word", "place", "frequency"]
+        fields = ["url", "word", "place", "frequency"]
