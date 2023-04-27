ce8ae071fa0530a5eea910f0d485695116b58c2f
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-04-28 01:06:45 +0200

    Increase pagination size

--- voices/settings.py
+++ voices/settings.py
@@ -177,7 +177,7 @@ LOGGING = {
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 1000,
 }
 
 JAZZMIN_UI_TWEAKS = {
