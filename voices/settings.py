3f0f34633d3ae6fa5034b74a79e021606dbdda0c
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-04-27 22:56:24 +0200

    Move django speech app inside voices

--- voices/settings.py
+++ voices/settings.py
@@ -42,7 +42,7 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost", "voci.afor.dev"]
 INSTALLED_APPS = [
     "corsheaders",
     "jazzmin",
-    "speech.apps.SpeechConfig",
+    "voices.speech.apps.SpeechConfig",
     "website.apps.WebsiteConfig",
     "django.contrib.admin",
     "django.contrib.auth",
