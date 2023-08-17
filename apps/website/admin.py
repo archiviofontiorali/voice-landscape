547d82f06780658fbd08c05da283622771e977b0
Elia Giacobazzi <elia.giacobazzi@euler.it>
2023-08-17 14:02:41 +0200

    Add settings for default point in location admin widgets

--- apps/website/admin.py
+++ apps/website/admin.py
@@ -1,15 +1,26 @@
+from django.conf import settings
 from django.contrib.gis import admin
 
 from . import models
 
 
+class LocationGISModel(admin.GISModelAdmin):
+    gis_widget_kwargs = {
+        "attrs": {
+            "default_zoom": 11,
+            "default_lon": settings.DEFAULT_POINT_LONGITUDE,
+            "default_lat": settings.DEFAULT_POINT_LATITUDE,
+        },
+    }
+
+
 @admin.register(models.Share)
-class ShareAdmin(admin.GISModelAdmin):
+class ShareAdmin(LocationGISModel):
     list_display = ("timestamp", "location", "message")
 
 
 @admin.register(models.Place)
-class PlaceAdmin(admin.GISModelAdmin):
+class PlaceAdmin(LocationGISModel):
     prepopulated_fields = {"slug": ("title",)}
     list_display = ("__str__", "title", "location", "description")
 
