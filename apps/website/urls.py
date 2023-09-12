4d392975b1a70ebb9b6910cad4715fa7cb246fa7
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-09-13 01:44:32 +0200

    Fix share url

--- apps/website/urls.py
+++ apps/website/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # path("", RedirectView.as_view(url="map/")),
     path("map/", views.MapPage.as_view(), name="map"),
     path("map/<slug:slug>", views.MapPage.as_view(), name="map"),
-    path("share", views.SharePage.as_view(), name="share"),
+    path("share/", views.SharePage.as_view(), name="share"),
     path("share/<slug:slug>", views.SharePage.as_view(), name="share"),
 ]
