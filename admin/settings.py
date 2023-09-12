f781b8f7756e2d4500f3ba45163cc582049fb03e
Elia Giacobazzi <elia.giacobazzi@euler.it>
2023-09-12 20:06:25 +0200

    Add template folder outside apps

--- admin/settings.py
+++ admin/settings.py
@@ -96,7 +96,9 @@ ROOT_URLCONF = "admin.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
