19ee0b358f0e48ee562d9a34e3b9b2fb6f59d86d
Elia Giacobazzi <elia.giacobazzi@euler.it>
2023-09-14 19:07:28 +0200

    Improve help text in share page

--- apps/website/forms.py
+++ apps/website/forms.py
@@ -7,8 +7,8 @@ class ShareForm(django.forms.Form):
     message = CharField(
         label=_("Esprimi la tua voce"),
         help_text=_(
-            "Scrivi il tuo pensiero qui o usa il tasto microfono per "
-            "registrare la tua voce"
+            "Scrivi il tuo pensiero qui o usa il microfono per "
+            "registrare e trascrivere la tua voce (max 30 secondi)"
         ),
         widget=Textarea,
         max_length=500,  # TODO: find a way to use same value in form and model
