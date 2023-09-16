32653d229c966dd01176253a540e8b8549e12e07
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-09-16 22:34:29 +0200

    Add a datetime converter

--- apps/website/urls.py
+++ apps/website/urls.py
@@ -1,7 +1,23 @@
-from django.urls import path
-from django.views.generic import RedirectView
+from django.urls import path, register_converter
+from django.utils import timezone
 
 from . import views
+
+
+class DateTimeConverter:
+    regex = r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}"
+
+    @staticmethod
+    def to_python(value):
+        time = timezone.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S")
+        return timezone.make_aware(time)
+
+    @staticmethod
+    def to_url(value):
+        return value
+
+
+register_converter(DateTimeConverter, "datetime")
 
 app_name = "website"
 urlpatterns = [
