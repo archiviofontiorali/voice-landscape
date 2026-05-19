8f33381ce6e92d7d602c84dfa9bed49f8df31099
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2026-05-19 19:31:40 +0200

    Move and update fields.py

--- app/website/fields.py
+++ app/website/fields.py
@@ -10,12 +10,12 @@ class UniqueBooleanField(models.BooleanField):
     def pre_save(self, model_instance, add):
         objects = model_instance.__class__.objects
 
-        if not objects.exclude(id=model_instance.id).filter(**{self.attname: True}):
+        if not objects.exclude(pk=model_instance.pk).filter(**{self.attname: True}):
             # If no true object exists different from saved model, set as True
             return True
 
         if value := getattr(model_instance, self.attname):
             # If value is True then set all others as False
-            objects.update(**{self.attname: False})
+            objects.exclude(pk=model_instance.pk).filter(**{self.attname: False})
 
         return value
