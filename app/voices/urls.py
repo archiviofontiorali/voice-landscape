3569e1ee1200fbba902fbb82d17e828fb7cb5936
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2026-05-19 19:36:10 +0200

    Move urls

--- app/voices/urls.py
+++ app/voices/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path("", include("website.urls", namespace="website")),
     path("admin/", admin.site.urls),
 ]
--- app/website/urls.py
+++ app/website/urls.py
@@ -20,10 +20,12 @@ class DateTimeConverter:
 register_converter(DateTimeConverter, "datetime")
 
 app_name = "website"
+
 urlpatterns = [
-    path("", views.Map.as_view()),
-    path("map/", views.Map.as_view(), name="map"),
-    path("share/", views.Share.as_view(), name="share"),
-    path("history/", views.HistoryMap.as_view(), name="history"),
-    path("history/<datetime:timestamp>/", views.HistoryMap.as_view(), name="history"),
+    path("", views.index),
+    # path("", views.Map.as_view()),
+    # path("map/", views.Map.as_view(), name="map"),
+    # path("share/", views.Share.as_view(), name="share"),
+    # path("history/", views.HistoryMap.as_view(), name="history"),
+    # path("history/<datetime:timestamp>/", views.HistoryMap.as_view(), name="history"),
 ]
